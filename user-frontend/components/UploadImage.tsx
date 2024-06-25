a668698c3a5562f7845c78d4eca475c477bf7094
pavan-dulam <pavandulam16@gmail.com>
2024-06-25 14:29:23 +0530

    added: config and completed uploadImage logic

--- user-frontend/components/UploadImage.tsx
+++ user-frontend/components/UploadImage.tsx
@@ -1,15 +1,74 @@
 'use client';
 
 import { useState } from 'react';
+import axios from 'axios';
+import { BACKEND_URL, CLOUDFRONT_URL } from '@/config';
 
-export const UploadImage = () => {
-	const [image, setImage] = useState('');
+export async function UploadImage({
+	onImageAdded,
+	image
+}: {
+	onImageAdded: (image: string) => void;
+	image?: string;
+}) {
+	const [uploading, setUploading] = useState(false);
+
+	async function onFileSelect(e: any) {
+		setUploading(true);
+		try {
+			const file = e.target.files[0];
+			const response = await axios.get(
+				`${BACKEND_URL}/v1/user/presignedUrl`,
+				{
+					headers: {
+						Authorization: localStorage.getItem('token')
+					}
+				}
+			);
+
+			const preSignedUrl = response.data.preSignedUrl;
+			const formData = new FormData();
+
+			formData.set('bucket', response.data.fields['bucket']);
+			formData.set(
+				'X-Amz-Algorithm',
+				response.data.fields['X-Amz-Algorithm']
+			);
+			formData.set(
+				'X-Amz-Credential',
+				response.data.fields['X-Amz-Credential']
+			);
+			formData.set(
+				'X-Amz-Algorithm',
+				response.data.fields['X-Amz-Algorithm']
+			);
+			formData.set('X-Amz-Date', response.data.fields['X-Amz-Date']);
+			formData.set('key', response.data.fields['key']);
+			formData.set('Policy', response.data.fields['Policy']);
+			formData.set(
+				'X-Amz-Signature',
+				response.data.fields['X-Amz-Signature']
+			);
+			formData.set(
+				'X-Amz-Algorithm',
+				response.data.fields['X-Amz-Algorithm']
+			);
+			formData.append('file', file);
+
+			const awsResponse = await axios.post(preSignedUrl, formData);
+			onImageAdded(`${CLOUDFRONT_URL}/${response.data.fields['key']}`);
+		} catch (e) {
+			console.log('error:', e);
+		}
+		setUploading(false);
+	}
 	return (
 		<div className='w-40 h-40 rounded border text-2xl cursor'>
-			<div className='h-full flex justify-center'>
-				<div className='h-full flex justify-center flex-col'>
+			<div className='h-full flex justify-center relative w-full'>
+				<div className='h-full flex justify-center w-full pt-16 text-4xl'>
 					+
 					<input
+						className='w-full h-full bg-red-400 w-40 h-40'
 						type='file'
 						style={{
 							opacity: 0,
@@ -21,12 +80,10 @@ export const UploadImage = () => {
 							width: '100%',
 							height: '100%'
 						}}
-						onSelect={(file) => {
-							console.log('file=', file);
-						}}
+						onChange={onFileSelect}
 					/>
 				</div>
 			</div>
 		</div>
 	);
-};
+}
